939cdd5e2313e915ea205a5439ec22443dfe180d
Gabriel Hurley <gabriel.hurley-ramstad@nasa.gov>
2011-05-31 12:48:25 -0700

    Adding base django comment URLs through block-comment URLS.

--- block_comment/urls.py
+++ block_comment/urls.py
@@ -3,3 +3,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('block_comment.views',
     url(r'^post_comment$', 'post_block_comment', name='post_block_comment'),
 )
+
+urlpatterns += patterns('',
+    url(r'', include('django.contrib.comments.urls')),
+)
