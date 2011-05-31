a651e5a02a185778e93373310cd4701555aaec48
Gabriel Hurley <gabriel.hurley-ramstad@nasa.gov>
2011-05-31 16:44:57 -0700

    Includes the "include" import...

--- block_comment/urls.py
+++ block_comment/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, url, include
 
 urlpatterns = patterns('block_comment.views',
     url(r'^post_comment$', 'post_block_comment', name='post_block_comment'),
